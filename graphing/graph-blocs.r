ed57206063610ca8d1652be001e9d85537fae382
Nixinova <nixinova@gmail.com>
2023-01-24 20:59:38 +1300

    graphing: Add graphing of political blocs
    Graphs the Left bloc (LAB+GRN) with the Right bloc (NAT+ACT).

---
+++ graphing/graph-blocs.r
@@ -0,0 +1,60 @@
+# In RStudio, Ctrl+A then Run
+
+library(ggplot2)
+library(dplyr)
+
+setwd("~/GitHub/nzpolls/graphing") # replace with own working directory
+
+# Read data
+csvData <- read.csv("local-data.csv")
+pollingData <- arrange(csvData, desc(as.Date(endDate, '%Y-%m-%d')))
+
+# Plot data
+spansize <- 0.5 # higher = smoother
+ggplot(pollingData, aes(x = as.Date(endDate, '%Y-%m-%d'))) +
+  theme_bw() +
+
+# Left
+geom_point(aes(y = left, colour = "Left"), size = 2, alpha = 0.3) +
+  geom_smooth(aes(y = left, colour = "Left"), span = spansize, se = FALSE) +
+# Right
+geom_point(aes(y = right, colour = "Right"), size = 2, alpha = 0.3) +
+  geom_smooth(aes(y = right, colour = "Right"), span = spansize, se = FALSE) +
+# Other
+geom_point(aes(y = other, colour = "Other"), size = 2, alpha = 0.3) +
+  geom_smooth(aes(y = other, colour = "Other"), span = spansize, se = FALSE) +
+
+# Y-axis
+scale_y_continuous(limits = c(0, 70), breaks = c(0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70), minor_breaks = waiver(), expand = c(0, 0)) +
+# X-axis
+scale_x_date(date_breaks = "4 months", date_labels = "%b '%y", minor_breaks = "1 month") +
+# Axis styling
+theme(
+    plot.background = element_rect(fill = "white", color = NA),
+    axis.text.x = element_text(angle = 0, vjust = 0.5, size = 10),
+    axis.text.y = element_text(size = 12),
+    axis.title.y = element_text(size = 12)
+  ) +
+# Axis labels
+labs(y = "Combined party vote (%)", x = NULL) +
+# Colors and key
+scale_color_manual(
+    name = "",
+# Legend
+    labels = c(
+      "Left" = "LAB+GRN",
+      "Right" = "NAT+ACT"
+    ),
+# Color mapping
+    values = c(
+      "Left" = "#B85300",
+      "Right" = "#5000B8",
+      "Other" = "#333333"
+    )
+  ) +
+  theme(
+    legend.position = "bottom",
+    legend.text = element_text(size = 12)
+  )
+
+# Save as 800x500 SVG
--- graphing/index.js
+++ graphing/index.js
@@ -6,19 +6,24 @@ const fs = require('fs');
 const yaml = require('js-yaml');
 const dateToString = date => date.toISOString().split('T')[0];
 
+const args = process.argv.slice(1);
+
 // Load data
 const baseFolder = __dirname + '/';
 const rawData = fs.readFileSync(baseFolder + '../data.yml', { encoding: 'utf8' });
 const data = yaml.load(rawData);
 
 // Check year input
-const year = process.argv[2] || 2023;
-if (!data[year]) throw `There is no data recorded for a ${year} election.`;
+const DEFAULT_ELECTION_YEAR = 2023;
+const year = /\d{4}/.test(args[1]) ? args[1] : DEFAULT_ELECTION_YEAR;
+if (!data[year])
+    throw `There is no data recorded for a ${year} election.`;
 
 // Prepare output data
 const PARTIES = ['ACT', 'GRN', 'INM', 'INT', 'LAB', 'MNA', 'MRI', 'NAT', 'NCP', 'NZF', 'TOP', 'UNF'];
 const partyData = { date: [], org: [], n: [] };
-for (const party of PARTIES) partyData[party] = [];
+for (const party of PARTIES)
+    partyData[party] = [];
 
 // Parse data
 let count = 0;
@@ -39,13 +44,33 @@ for (const poll of data[year]) {
     // Pad data if party not listed in poll
     count++;
     for (const key in partyData) {
-        if (partyData[key].length === count) continue;
-        partyData[key].push('');
+        if (partyData[key].length === count)
+            continue;
+        partyData[key].push();
     }
 }
 // Fix crash when trying to plot future parties
 for (const party of PARTIES) {
-    if (partyData[party].length === 0) partyData[party] = [0];
+    if (partyData[party].length === 0)
+        partyData[party] = [0];
+}
+
+// Collate parties into blocs
+const BLOCS = {
+    'left': ['LAB', 'GRN'],
+    'right': ['NAT', 'ACT'],
+};
+BLOCS['other'] = PARTIES.filter(party => !BLOCS['left'].includes(party) && !BLOCS['right'].includes(party));
+for (const bloc of ['left', 'right', 'other']) {
+    partyData[bloc] = new Array(partyData.date.length).fill();
+    for (let i in partyData[bloc]) {
+        for (const party of BLOCS[bloc]) {
+            if (partyData[party][i]) {
+                partyData[bloc][i] ??= 0;
+                partyData[bloc][i] += partyData[party][i];
+            }
+        }
+    }
 }
 
 // Create CSV
@@ -56,9 +81,14 @@ csvData += ',' + dateToString(electionDate) + '\n';
 for (let i = 0; i < count; i++) {
     csvData += partyData.date[i].join(',') + ',';
     const pollData = [];
-    for (const party of PARTIES) pollData.push(partyData[party][i]);
+    for (const party of PARTIES)
+        pollData.push(partyData[party][i]);
+    for (const bloc in BLOCS) {
+        pollData.push(partyData[bloc][i]);
+    }
     csvData += pollData.join(',') + '\n';
 }
+csvData = csvData.replace(/NaN/g, '');
 
 const outFileName = baseFolder + 'local-data.csv';
 try { fs.rmSync(outFileName); } catch { }
